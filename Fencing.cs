9f54063098dac0101d2fbd8096df8c8599886100
Chris <chrismorrisrva@gmail.com>
2018-09-18 23:19:09 -0400

    Entire file added.

---
+++ Fencing.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace extraCredit2
+{
+    class Fencing
+    {
+        static void Main(){
+            double yardSize;
+            double length;
+            double height;
+            string value1;
+            string value2;
+            Console.WriteLine("Provide the length of the square-shaped yard in feet: ");
+            value1 = Console.ReadLine();
+            length = double.Parse(value1);
+            Console.ReadKey();
+            Console.WriteLine("Provide the height of the square-shaped yard in feet: ");
+            value2 = Console.ReadLine();
+            height = double.Parse(value2);
+            Console.ReadKey();
+            yardSize = length*height;
+            Console.WriteLine("You will need " + yardSize + " Square feet of fencing for the required perimeter of the yard.");
+        }
+    }
+}
